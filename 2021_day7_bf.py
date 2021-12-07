aaaeba694762a6641d04bb3b96bbbd401740a643
balazsmm <95389035+balazsmm@users.noreply.github.com>
2021-12-07 11:52:53 +0100

    Day 7 print disambiguation

--- 2021_day7_bf.py
+++ 2021_day7_bf.py
@@ -9,5 +9,5 @@ def min_pos_consumption(positions, cons_function):
 	min_value = [f for f in cons_dict if cons_dict[f] == min_fuel][0]
 	return  min_fuel, min_value
 
-print("Part 1: Min fuel {}, min value {}".format(*min_pos_consumption(positions, int)))
-print("Part 2: Min fuel {}, min value {}".format(*min_pos_consumption(positions, cumulative_consumption)))
+print("Part 1: Min fuel {}, position for min fuel {}".format(*min_pos_consumption(positions, int)))
+print("Part 2: Min fuel {}, position for min fuel {}".format(*min_pos_consumption(positions, cumulative_consumption)))
